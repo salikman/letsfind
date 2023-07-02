cc97c11525531560ef6d7eaaa70ec1456d96d6b6
Max Savonik <savonikmaksim@gmail.com>
2023-07-02 14:55:12 +0300

    fix home

--- src/js/app.js
+++ src/js/app.js
@@ -16,62 +16,153 @@ for (let i = 0; i < descriptions.length; i++) {
 /* ********************************************************************** */
 /* ********************************Swiper******************************** */
 const swiperServices = new Swiper(".services-slider", {
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    }
   },
 });
 const swiperWorks = new Swiper(".works-slider", {
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    }
   },
 });
 const swiperHouse = new Swiper(".house-slider", {
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    }
   },
 });
 const swiperShopping = new Swiper(".shopping-slider", {
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    }
   },
 });
 const swiperAnnounc = new Swiper(".announc-slider", {
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    }
   },
 });
 const swiperPeople = new Swiper(".people-slider", {
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    }
   },
 });
 const swiperSale = new Swiper(".sale-slider", {
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
   },
+  breakpoints: {
+    540: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    }
+  },
 });
 const swiperBlog = new Swiper(".blog-slider", {
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    }
   },
 });
 /* ********************************************************************** */
 /* ******************************Materialize***************************** */
 const elTabs = document.querySelectorAll('.tabs')
 const iTabs = M.Tabs.init(elTabs);
+
+const eSidenav = document.querySelectorAll('.sidenav');
+const iSidenav = M.Sidenav.init(eSidenav);
