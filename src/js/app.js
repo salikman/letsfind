6df8405653f3ddc618b95b86657e068858fa416b
Maxym Savonik <msavonik@salerow.software>
2023-07-04 11:56:02 +0300

    page post-job

--- src/js/app.js
+++ src/js/app.js
@@ -15,133 +15,7 @@ for (let i = 0; i < descriptions.length; i++) {
 }
 /* ********************************************************************** */
 /* ********************************Swiper******************************** */
-const swiperServices = new Swiper(".services-slider", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  breakpoints: {
-    540: {
-      slidesPerView: 2,
-    },
-    640: {
-      slidesPerView: 3,
-    },
-    992: {
-      slidesPerView: 4,
-    }
-  },
-});
-const swiperWorks = new Swiper(".works-slider", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  breakpoints: {
-    540: {
-      slidesPerView: 2,
-    },
-    640: {
-      slidesPerView: 3,
-    },
-    992: {
-      slidesPerView: 4,
-    }
-  },
-});
-const swiperHouse = new Swiper(".house-slider", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  breakpoints: {
-    540: {
-      slidesPerView: 2,
-    },
-    640: {
-      slidesPerView: 3,
-    },
-    992: {
-      slidesPerView: 4,
-    }
-  },
-});
-const swiperShopping = new Swiper(".shopping-slider", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  breakpoints: {
-    540: {
-      slidesPerView: 2,
-    },
-    640: {
-      slidesPerView: 3,
-    },
-    992: {
-      slidesPerView: 4,
-    }
-  },
-});
-const swiperAnnounc = new Swiper(".announc-slider", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  breakpoints: {
-    540: {
-      slidesPerView: 2,
-    },
-    640: {
-      slidesPerView: 3,
-    },
-    992: {
-      slidesPerView: 4,
-    }
-  },
-});
-const swiperPeople = new Swiper(".people-slider", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  breakpoints: {
-    540: {
-      slidesPerView: 2,
-    },
-    640: {
-      slidesPerView: 3,
-    },
-    992: {
-      slidesPerView: 4,
-    }
-  },
-});
-const swiperSale = new Swiper(".sale-slider", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  breakpoints: {
-    540: {
-      slidesPerView: 2,
-    },
-    640: {
-      slidesPerView: 3,
-    },
-    992: {
-      slidesPerView: 4,
-    }
-  },
-});
-const swiperBlog = new Swiper(".blog-slider", {
+const swiperServices = new Swiper(".section-js", {
   slidesPerView: 1,
   spaceBetween: 20,
   pagination: {
@@ -166,3 +40,9 @@ const iTabs = M.Tabs.init(elTabs);
 
 const eSidenav = document.querySelectorAll('.sidenav');
 const iSidenav = M.Sidenav.init(eSidenav);
+
+const eModal = document.querySelectorAll('.modal');
+const iModal = M.Modal.init(eModal);
+
+const eSelect = document.querySelectorAll('select');
+const iSelect = M.FormSelect.init(eSelect);
